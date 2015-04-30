218751aabce209f8ed345028f7e2486f23e12231
Greg <smithgreg1983@gmail.com>
2015-04-30 14:44:31 -0600

    Code cleanup

--- js/src/alertuser.js
+++ js/src/alertuser.js
@@ -1,11 +1,8 @@
-// Return a flag (could be a string or a boolean) to the "onsubmit" part of form in renderer
-// Should be returned to renderer. Might also have to add (or edit) to studentview:89
-
 document.addEventListener("DOMContentLoaded", function(event) { 
 
 });
 
-
+// Return a flag (could be a string or a boolean) to the "onsubmit" part of form in renderer
 function alert_user()
 {
 	var choice = false
